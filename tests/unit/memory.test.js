e60b8959dafc05acd6fac8d3a809e8316264c789
srpurba <srpurba@myseneca.ca>
2022-10-02 03:26:48 -0400

    covered memory.test.js

--- tests/unit/memory.test.js
+++ tests/unit/memory.test.js
@@ -15,16 +15,56 @@ describe('fragment database related calls', () => {
 
   test('writeFragmentData() returns nothing', async () => {
     const rawData = Buffer.from([], 'utf8');
-    const fragment = { ownerId: '', id: '', value: rawData };
-    const result = await memory.writeFragmentData(fragment.ownerId, fragment.id, fragment.value);
+    const fragmentData = { ownerId: '', id: '', value: rawData };
+    const result = await memory.writeFragmentData(
+      fragmentData.ownerId,
+      fragmentData.id,
+      fragmentData.value
+    );
     expect(result).toBe(undefined);
   });
 
   test('readFragmentData() returns what we writeFragmentData() into the db', async () => {
     const bufferStr = Buffer.from('somefile', 'utf8');
-    const fragment = { ownerId: '77', id: '777', value: bufferStr };
-    await memory.writeFragmentData(fragment.ownerId, fragment.id, fragment.value);
-    const dbResult = await memory.readFragmentData(fragment.ownerId, fragment.id);
-    expect(dbResult).toEqual(fragment.value);
+    const fragmentData = { ownerId: '77', id: '777', value: bufferStr };
+    await memory.writeFragmentData(fragmentData.ownerId, fragmentData.id, fragmentData.value);
+    const dbResult = await memory.readFragmentData(fragmentData.ownerId, fragmentData.id);
+    expect(dbResult).toEqual(fragmentData.value);
   });
+
+  test('listFragments() returns all fragmentMetada', async () => {
+    const fragmentMetadata = { ownerId: '77', id: '777', fragment: '7777' };
+    await memory.writeFragment(fragmentMetadata);
+    const results = await memory.listFragments('77', true); // true = include data
+    expect(results).toEqual([{ ...fragmentMetadata }]);
+  });
+
+  test('listFragments() return only array of ids', async () => {
+    const fragmentMetadata = { ownerId: '77', id: '777', fragment: '7777' };
+    await memory.writeFragment(fragmentMetadata);
+    const results = await memory.listFragments('77', false); // false = only ids
+    expect(results).toEqual([fragmentMetadata.id]);
+  });
+
+  test('deleteFragment() removes value what writeFragment() writes into db', async () => {
+    const fragmentMetadata = { ownerId: '77', id: '777', fragment: '7777' };
+    await memory.writeFragment(fragmentMetadata);
+    expect(await memory.readFragment(fragmentMetadata.ownerId, fragmentMetadata.id)).toEqual(
+      fragmentMetadata
+    );
+    await memory.deleteFragment(fragmentMetadata.ownerId, fragmentMetadata.id);
+    expect(await memory.readFragment(fragmentMetadata.ownerId, fragmentMetadata.id)).toBe(
+      undefined
+    );
+  });
+
+  test('deleteFragment() indeed throws', async () => {
+    const fragmentMetadata = { ownerId: '77', id: '777', fragment: '7777' };
+    await memory.writeFragment(fragmentMetadata);
+    expect(await memory.readFragment(fragmentMetadata.ownerId, fragmentMetadata.id)).toEqual(
+      fragmentMetadata
+    );
+    await expect(memory.deleteFragment(fragmentMetadata.ownerId, 'nonexistent')).rejects.toThrow();
+  });
+
 });
