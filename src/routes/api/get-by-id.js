3bc093ffb21d687ff03f47bb6e9c46e599cf8abb
srpurba <srpurba@myseneca.ca>
2022-10-08 18:14:54 -0400

    updated getbyid with appropriate logs

--- src/routes/api/get-by-id.js
+++ src/routes/api/get-by-id.js
@@ -5,14 +5,14 @@ const logger = require('../../logger');
 module.exports = async (req, res) => {
   try {
     const fragment = await Fragment.byId(req.user, req.params.id);
-    const fragmentData = await fragment.getData().then((data) => data.toString());
+    const fragmentData = await fragment.getData();
+    // buffer to string
+    const strData = fragmentData.toString('utf8');
     res
       .status(200)
-      .json(
-        createSuccessResponse({ size: fragment.size, type: fragment.type, data: fragmentData })
-      );
+      .json(createSuccessResponse({ size: fragment.size, type: fragment.type, data: strData }));
   } catch (err) {
     res.status(404).json(createErrorResponse(404, 'Fragment not found'));
-    logger.error('Fragment not found: ' + err);
+    logger.error(`Fragment id ${req.params.id} does not exist` + err);
   }
 };
