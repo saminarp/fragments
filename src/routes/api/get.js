a071d3fb65c2970c7137f682ab8b27c9cfef43b4
srpurba <srpurba@myseneca.ca>
2022-10-08 18:08:00 -0400

    updated GET route with expand option
    currently its hardcoded to false

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -1,10 +1,17 @@
 // src/routes/api/get.js
-const { createSuccessResponse } = require('../../response');
+const { createSuccessResponse, createErrorResponse } = require('../../response');
 const { Fragment } = require('../../model/fragment');
+const logger = require('../../logger');
 /**
  * Get a list of fragments for the current user
  */
 module.exports = async (req, res) => {
-  const fragment = await Fragment.byUser(req.user);
-  res.status(200).json(createSuccessResponse({ fragment }));
+  try {
+    const fragment = await Fragment.byUser(req.user, false);
+    logger.info('GET request for fragment data: ', { user: req.user, fragment });
+    res.status(200).json(createSuccessResponse({ fragment }));
+  } catch (error) {
+    logger.error(error);
+    res.status(500).json(createErrorResponse(error));
+  }
 };
