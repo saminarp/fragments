1ddccb824ee68872208c7220de1fcd06daa8b983
srpurba <srpurba@myseneca.ca>
2022-10-08 22:07:39 -0400

    updated routes and logging

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -26,6 +26,7 @@ const rawBody = () =>
 
 // Define our first route, which will be: GET /v1/fragments
 router.get('/fragments', require('./get'));
+
 router.get('/fragments/:id', require('./get-by-id'));
 // Other routes will go here later on...
 
--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -8,10 +8,11 @@ module.exports = async (req, res) => {
     const fragment = new Fragment({ ownerId: req.user, type: req.get('Content-Type') });
     await fragment.save();
     await fragment.setData(req.body);
+    logger.info(`Created fragment data for user ${req.user}`, { fragment });
     res.set('Location', `${API_URL}/v1/fragments/${fragment.id}`);
     res.status(201).json(createSuccessResponse({ fragment }));
   } catch (error) {
-    logger.warn('Fragment cannot be created due to ' + error);
+    logger.error('Fragment cannot be created due to ' + error);
     return res.status(500).json(createErrorResponse(500, error.message));
   }
 };
