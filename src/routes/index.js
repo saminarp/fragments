3129f3a09aa57f04fe046b71357f1fb92cb277d2
srpurba <srpurba@myseneca.ca>
2022-11-25 02:42:40 -0500

    add hostname to healthcheck

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const { createSuccessResponse } = require('../response');
 // version and author from package.json
 const { githubUrl, version, author } = require('../../package.json');
-
+const { hostname } = require('os');
 // Create a router that we can use to mount our API
 const router = express.Router();
 // authorization middleware
@@ -16,6 +16,7 @@ const data = {
   githubUrl,
   version,
   author,
+  hostname: hostname(),
 };
 /**
  * Expose all of our API routes on /v1/* to include an API version.
@@ -26,10 +27,15 @@ router.use(`/v1`, authenticate(), require('./api'));
  * we'll respond with a 200 OK.  If not, the server isn't healthy.
  */
 
+// router.get('/', (req, res) => {
+//   // Client's shouldn't cache this response (always request it fresh)
+//   res.setHeader('Cache-Control', 'no-cache');
+//   // Send a 200 'OK' response
+//   res.status(200).json(createSuccessResponse(data));
+// });
+
 router.get('/', (req, res) => {
-  // Client's shouldn't cache this response (always request it fresh)
   res.setHeader('Cache-Control', 'no-cache');
-  // Send a 200 'OK' response
   res.status(200).json(createSuccessResponse(data));
 });
 
