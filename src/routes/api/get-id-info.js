803444800eb24204e217d8f073c3f7487cc08037
srpurba <srpurba@myseneca.ca>
2022-12-03 00:53:02 -0500

    get by id routes updated

--- src/routes/api/get-id-info.js
+++ src/routes/api/get-id-info.js
@@ -6,7 +6,7 @@ module.exports = async (req, res) => {
   try {
     const fragment = await Fragment.byId(req.user, req.params.id);
     if (!fragment) return res.status(404).json(createErrorResponse(404, 'Not Found'));
-    res.status(200).json(createSuccessResponse(fragment)); // returns Fragment Metadata
+    res.status(200).json(createSuccessResponse({ fragment: fragment })); // returns Fragment Metadata
   } catch (err) {
     logger.error({ err }, 'Error getting fragment by id');
     res.status(404).json(createErrorResponse(404, 'Error getting fragment information'));
